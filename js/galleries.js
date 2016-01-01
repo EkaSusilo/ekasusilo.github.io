894a6e54bdef58db953f5949bd9b02f896872ce8
Ekawahyu Susilo <ekawahyu@aisenke.com>
2016-01-01 11:07:48 -0600

    start updating portfolio content

--- js/galleries.js
+++ js/galleries.js
@@ -6,124 +6,26 @@ jQuery('#gallery1').on('click', function (e) {
         // Settings
         dynamic: true,
         mode: 'lg-slide-vertical',
-        zoom: true,
-        fullScreen: true,
-        autoplay: false,
-        thumbnail: true,
-        download: true,
-        counter: true,
-        // Images
-        dynamicEl: [{
-            'src': 'images/photos/800.png',
-            'thumb': 'images/photos/150.png',
-            'subHtml': 'Lorem ipsum dolor'
-        }, {
-            'src': 'images/photos/800.png',
-            'thumb': 'images/photos/150.png',
-            'subHtml': "Ullamco ubi mandaremus"
-        }, {
-            'src': 'images/photos/800.png',
-            'thumb': 'images/photos/150.png',
-            'subHtml': "Quae expetendis"
-        }, {
-            'src': 'images/photos/800.png',
-            'thumb': 'images/photos/150.png',
-            'subHtml': "Non nulla"
-        }]
-    });
-});
-
-// GALLERY 2
-jQuery('#gallery2').on('click', function (e) {
-    "use strict";
-    e.preventDefault();
-    jQuery(this).lightGallery({
-        // Settings
-        dynamic: true,
-        mode: 'lg-slide-vertical',
-        zoom: false,
-        fullScreen: false,
-        autoplayControls: false,
-        thumbnail: false,
-        download: true,
-        counter: true,
-        // Videos
-        dynamicEl: [
-            {
-                "src": "http://vimeo.com/70301553",
-                "subHtml": "Vimeo"
-            },
-            { // You Tube videos work only on a server.
-                "src": "http://www.youtube.com/embed/efVWyPNd3xw",
-                "subHtml": "You Tube"
-            }
-        ]
-    });
-});
-
-// GALLERY 3
-jQuery('#gallery3').on('click', function (e) {
-    "use strict";
-    e.preventDefault();
-    jQuery(this).lightGallery({
-        // Settings
-        dynamic: true,
-        mode: 'lg-slide-vertical',
         zoom: false,
         fullScreen: false,
         autoplay: true,
-        pause: 5000,
+        pause: 4000,
         thumbnail: false,
         download: false,
         counter: true,
         // Images
         dynamicEl: [{
-            'src': 'images/photos/800.png',
-            'thumb': 'images/photos/150.png',
-            'subHtml': 'Quae expetendis'
+            'src': 'images/portfolio/bldc1_800.jpg',
+            'subHtml': 'Four layers rigid-flex material'
         }, {
-            'src': 'images/photos/800.png',
-            'thumb': 'images/photos/150.png',
-            'subHtml': "Lorem ipsum dolor"
+            'src': 'images/portfolio/bldc2_800.jpg',
+            'subHtml': 'Board thickness is 0.5mm'
         }, {
-            'src': 'images/photos/800.png',
-            'thumb': 'images/photos/150.png',
-            'subHtml': "Legam a expetendis"
+            'src': 'images/portfolio/bldc3_800.jpg',
+            'subHtml': 'Board diameter is 9.7mm'
         }, {
-            'src': 'images/photos/800.png',
-            'thumb': 'images/photos/150.png',
-            'subHtml': "Magna quamquam"
-        }]
-    });
-});
-
-// GALLERY 4
-jQuery('#gallery4').on('click', function (e) {
-    "use strict";
-    e.preventDefault();
-    jQuery(this).lightGallery({
-        // Settings
-        dynamic: true,
-        mode: 'lg-slide-vertical',
-        zoom: true,
-        fullScreen: true,
-        autoplay: false,
-        thumbnail: true,
-        download: true,
-        counter: true,
-        // Images
-        dynamicEl: [{
-            'src': 'images/photos/800.png',
-            'thumb': 'images/photos/150.png',
-            'subHtml': 'Tamen excepteur'
-        }, {
-            'src': 'images/photos/800.png',
-            'thumb': 'images/photos/150.png',
-            'subHtml': "Iudicem admodum"
-        }, {
-            'src': 'images/photos/800.png',
-            'thumb': 'images/photos/150.png',
-            'subHtml': "Enim pariatur"
+            'src': 'images/portfolio/bldc4_800.jpg',
+            'subHtml': 'BLDC controller for two motors'
         }]
     });
 });

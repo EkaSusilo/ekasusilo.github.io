31c2e8f2523c520ea291b997b8abef97b9ceee1e
Ekawahyu Susilo <ekawahyu@aisenke.com>
2016-01-02 04:09:40 -0600

    adding youtube video on portfolio

--- js/galleries.js
+++ js/galleries.js
@@ -1,5 +1,37 @@
-// GALLERY 1
-jQuery('#gallery1').on('click', function (e) {
+// GALLERY miniature BLDC controller
+jQuery('#gallery_bldc').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: false,
+        fullScreen: false,
+        autoplay: false,
+        pause: 4000,
+        thumbnail: false,
+        download: false,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/portfolio/bldc1_800.jpg',
+            'subHtml': 'Four layers rigid-flex material'
+        }, {
+            'src': 'images/portfolio/bldc2_800.jpg',
+            'subHtml': 'Board thickness is 0.5mm'
+        }, {
+            'src': 'images/portfolio/bldc3_800.jpg',
+            'subHtml': 'Board diameter is 9.7mm'
+        }, { // You Tube videos work only on a server.
+            'src': 'https://www.youtube.com/watch?v=x5mda9hJNuQ',
+            'subHtml': 'Controling Namiki 2mm diameter BLDC motor'
+        }]
+    });
+});
+
+// GALLERY Miniature wireless BLDC controller
+jQuery('#gallery_wbldc').on('click', function (e) {
     "use strict";
     e.preventDefault();
     jQuery(this).lightGallery({
@@ -15,17 +47,52 @@ jQuery('#gallery1').on('click', function (e) {
         counter: true,
         // Images
         dynamicEl: [{
-            'src': 'images/portfolio/bldc1_800.jpg',
-            'subHtml': 'Four layers rigid-flex material'
+            'src': 'images/portfolio/wbldc1_800.jpg',
+            'subHtml': 'Four layers FR4 material'
         }, {
-            'src': 'images/portfolio/bldc2_800.jpg',
+            'src': 'images/portfolio/wbldc2_800.jpg',
             'subHtml': 'Board thickness is 0.5mm'
         }, {
-            'src': 'images/portfolio/bldc3_800.jpg',
+            'src': 'images/portfolio/wbldc3_800.jpg',
             'subHtml': 'Board diameter is 9.7mm'
         }, {
-            'src': 'images/portfolio/bldc4_800.jpg',
-            'subHtml': 'BLDC controller for two motors'
+            'src': 'images/portfolio/wbldc4_800.jpg',
+            'subHtml': 'Wireless controller is energy efficient'
+        }, {
+            'src': 'images/portfolio/wbldc5_800.jpg',
+            'subHtml': 'It is an IoT device powered by a coin battery'
+        }, {
+            'src': 'images/portfolio/wbldc6_800.jpg',
+            'subHtml': 'Actual size of BLDC wireless controller'
+        }]
+    });
+});
+
+// GALLERY Voice command capsule robot
+jQuery('#gallery_voice_command').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: false,
+        fullScreen: false,
+        autoplay: false,
+        pause: 4000,
+        thumbnail: false,
+        download: false,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'https://www.youtube.com/watch?v=11lk4Q6Ex5g',
+            'subHtml': 'Voice command on capsule robot without a surgical mask'
+        }, { // You Tube videos work only on a server.
+            'src': 'https://www.youtube.com/watch?v=1JYSGsY_G3U',
+            'subHtml': 'Voice command on capsule robot while wearing a surgical mask'
+        }, { // You Tube videos work only on a server.
+            'src': 'https://www.youtube.com/watch?v=c6EiOUmYdBY',
+            "subHtml": "Voice command on capsule robot while wearing a 'very thick' surgical mask"
         }]
     });
 });
